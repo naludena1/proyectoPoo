1351978a467774be601d5306b0c763b028d01cb8
naludena1 <49170811+naludena1@users.noreply.github.com>
2020-05-12 21:49:46 -0500

    Add files via upload

--- ActividadExtraClase.java
+++ ActividadExtraClase.java
@@ -1,16 +1,26 @@
+
+import java.util.Random;
 public class ActividadExtraClase {
     public static void main(String[] args) {
-        int [] values = new int [10];
-        for(int i = 0; i < 10000000; i ++){
+        Random numAleatorio = new Random(System.currentTimeMillis());
+        int[] values = new int[100];
+
+        for (int i = 0; i < values.length; i++) {
+            values[i] = numAleatorio.nextInt(500);
+        }
+        for (int j = 0; j < 10000000; j++) {
             int index = -1;
-            for (int j = 0; j < values.length; j ++){
-                if (values [j] == 80){
-                    index = j;
+
+            for (int k = 0; k < values.length; k++) {
+                if (values[k] == 80) {
+                    index = k;
+
+                    System.out.printf("Numero(%d) = %d \n", index, values[k]);
                 }
-            }break;
+            }
+            break;
+
         }
 
-
     }
-
 }
